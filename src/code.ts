e1379d60720d3ddb5a538bc8cb920f9402dac9d3
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-01-24 02:24:42 -0800

    observer - fromEvent

--- src/code.ts
+++ src/code.ts
@@ -1,19 +1,13 @@
-import { Observable } from 'rxjs';
-import { share } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
 
-const observable = new Observable((subscriber) => {
-  try {
-    subscriber.next('Hey guys!');
-    subscriber.next('How are you?');
-    setInterval(() => {
-      subscriber.next('I am good');
-    }, 2000);
-    // subscriber.complete();
-    // subscriber.next('This will not send');
-  } catch (err) {
-    subscriber.error(err);
-  }
-}).pipe(share());
+const observable = fromEvent(document, 'mousemove');
+setTimeout(() => {
+  var observer = observable.subscribe({
+    next(x) {
+      addItem(x);
+    },
+  });
+});
 
 function addItem(val: any) {
   var node = document.createElement('li');
@@ -21,28 +15,3 @@ function addItem(val: any) {
   node.appendChild(textnode);
   document.getElementById('output').appendChild(node);
 }
-
-console.log('just before subscribe');
-var observer = observable.subscribe({
-  next(x) {
-    console.log('got value ' + x);
-    addItem(x);
-  },
-  error(err) {
-    console.error('something wrong occurred: ' + err);
-  },
-  complete() {
-    console.log('done');
-    addItem('Completed');
-  },
-});
-
-console.log('just after subscribe');
-
-setTimeout(() => {
-  var observer2 = observable.subscribe({
-    next(x) {
-      addItem('Subscriber 2: ' + x);
-    },
-  });
-}, 1000);
