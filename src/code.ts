ae9bbde2f0a20f16d347df8c46e03395ca90e763
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-01-24 02:47:21 -0800

    subject

--- src/code.ts
+++ src/code.ts
@@ -1,13 +1,23 @@
-import { fromEvent } from 'rxjs';
+import { Subject } from 'rxjs';
 
-const observable = fromEvent(document, 'mousemove');
-setTimeout(() => {
-  var observer = observable.subscribe({
-    next(x) {
-      addItem(x);
-    },
-  });
-});
+var subject = new Subject();
+
+subject.subscribe(
+  (data) => addItem('Observer 1: ' + data),
+  (err) => addItem(err),
+  () => addItem('Observer 1 Completed')
+);
+
+subject.next('The first thing has been sent');
+
+var observer2 = subject.subscribe((data) => addItem('Observer 2: ' + data));
+
+subject.next('The second thing has been sent');
+subject.next('The third thing has been sent');
+
+observer2.unsubscribe();
+
+subject.next('A final thing has been sent');
 
 function addItem(val: any) {
   var node = document.createElement('li');
